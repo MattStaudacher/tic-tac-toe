33b63d98de8cfb673f4281e0fefe6ad33d8b710e
Matt Staudacher and William Kinzig <matt@>
2018-06-13 09:41:06 -0700

    initialized files, created html structure for tic tac toe board.

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+/***************************** BACK END ******************************/
+function Player(name) {
+  this.name = name;
+
+}
+
+function Space() {
+
+}
+function Board() {
+
+}
+
+function Game() {
+
+}
+
+
+/*******************  FRONT END (USER INTERFACE) *********************/
+$(document).ready(function() {
+  var gameBoard = new Board();
+}
