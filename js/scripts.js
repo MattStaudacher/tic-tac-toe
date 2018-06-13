047c0ae84edfd63822b705b358f78c57fbde26ec
Matt Staudacher and William Kinzig <matt@>
2018-06-13 14:00:55 -0700

    added x o buttons to html for player choice.

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,9 @@ function Space() {
 
 }
 function Board() {
-
+  $("#board").show();
+  $(".player").show();
+// alert(boardArray.length);
 }
 
 function Game() {
@@ -33,19 +35,20 @@ function isOver(){
 
 }
 var boardArray = [
-                  [0,0],[0,1],[0,2]
-                  [1,0],[1,1],[1,2]
-                  [2,0],[2,1],[2,2]
-                                  ];
+                  [],[],[],
+                  [],[],[],
+                  [],[],[]
+                          ];
 
 /*******************  FRONT END (USER INTERFACE) *********************/
 $(document).ready(function() {
   var gameBoard = new Board();
 
+
+
 $("#0-0").click(function(){
   $("#0-0").text(whosTurn());  // fill user (visual) board with X or O
-  boardArray [0,0] = whosTurn(); // fill array/board with X or O
-  alert(boardArray [0]);
+  boardArray [0] = whosTurn(); // fill array/board with X or O
   counter++; // add counter to make it odd or even to determine whosTurn using %2
 
   //isOver();
@@ -53,49 +56,56 @@ $("#0-0").click(function(){
 
 $("#0-1").click(function(){
   $("#0-1").text(whosTurn());
+  boardArray [1] = whosTurn();
   counter++;
-  alert(counter);
+
 });
 
 $("#0-2").click(function(){
   $("#0-2").text(whosTurn());
+  boardArray [2] = whosTurn();
   counter++;
-  alert(counter);
+
 });
 
 $("#1-0").click(function(){
   $("#1-0").text(whosTurn());
+  boardArray [3] = whosTurn();
   counter++;
-  alert(counter);
+
 });
 
 $("#1-1").click(function(){
   $("#1-1").text(whosTurn());
+  boardArray [4] = whosTurn();
   counter++;
-  alert(counter);
+
 });
 
 $("#1-2").click(function(){
   $("#1-2").text(whosTurn());
+  boardArray [5] = whosTurn();
   counter++;
-  alert(counter);
+
 });
 
 $("#2-0").click(function(){
   $("#2-0").text(whosTurn());
+  boardArray [6] = whosTurn();
   counter++;
-  alert(counter);
+
 });
 
 $("#2-1").click(function(){
   $("#2-1").text(whosTurn());
+  boardArray [7] = whosTurn();
   counter++;
-  alert(counter);
+
 });
 
 $("#2-2").click(function(){
   $("#2-2").text(whosTurn());
+  boardArray [8] = whosTurn();
   counter++;
-  alert(counter);
 });
 });
