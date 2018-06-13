a4ebe32b2e7c2bb171f83f82d8ef4a5ce5402364
Matt Staudacher and William Kinzig <matt@>
2018-06-13 10:46:47 -0700

    it will add click function to our table.

--- js/scripts.js
+++ js/scripts.js
@@ -20,4 +20,34 @@ function Game() {
 /*******************  FRONT END (USER INTERFACE) *********************/
 $(document).ready(function() {
   var gameBoard = new Board();
-}
+
+$("#0-0").click(function(){
+  //alert("tester");
+  $("#0-0").text("X")
+
+});
+$("#0-1").click(function(){
+  alert("tester");
+});
+$("#0-2").click(function(){
+  alert("tester");
+});
+$("#1-0").click(function(){
+  alert("tester");
+});
+$("#1-1").click(function(){
+  alert("tester");
+});
+$("#1-2").click(function(){
+  alert("tester");
+});
+$("#2-0").click(function(){
+  alert("tester");
+});
+$("#2-1").click(function(){
+  alert("tester");
+});
+$("#2-2").click(function(){
+  alert("tester");
+});
+});
